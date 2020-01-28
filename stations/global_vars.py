85d218d1d397ac4e60da92981d2d2a3294d6ad10
themindstorm <parth.kabra@gmail.com>
2020-01-28 22:04:40 +0800

    added global vars

---
+++ stations/global_vars.py
@@ -0,0 +1,18 @@
+
+LINES = [
+
+  {'type': 'mrt', 'name': 'east-west', 'ref': 'ew', 'id': 445764},
+  {'type': 'mrt', 'name': 'north-south', 'ref': 'ns', 'id': 445768},
+  {'type': 'mrt', 'name': 'north-east', 'ref': 'ne', 'id': 2293545},
+  {'type': 'mrt', 'name': 'circle', 'ref': 'cc', 'id': 2076291},
+  {'type': 'mrt', 'name': 'downtown', 'ref': 'dt', 'id': 2313458},
+
+  # {'ref': 'bp', 'id': 1159434},
+  # {'ref': 'sw', 'id': 1146941},
+  # {'ref': 'se', 'id': 2312985},
+  # {'ref': 'pw', 'id': 2312984},
+  # {'ref': 'pe', 'id': 1146942},
+  # {'ref': 'cg', 'id': 2313372},
+
+  # {'ref': 's', 'id': 2353581},
+]
--- stations/stations.py
+++ stations/stations.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
+from global_vars import LINES
+# TODO ^
+
 from functions import get_line
 
 response = requests.get("https://api.openstreetmap.org/api/0.6/relation/445764/full")
